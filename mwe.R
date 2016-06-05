7727e84ec0cfc1500b8e587835cc3343fb4bf4ba
Dan Hicks <hicks.daniel.j@gmail.com>
2016-06-05 16:01:57 -0500

    fix the MWE

--- mwe.R
+++ mwe.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(survey)
+## https://cran.r-project.org/web/packages/srvyr/vignettes/srvyr-vs-survey.html
 library(survival)
 
 datafile = '2016-05-24.Rdata'
@@ -23,10 +24,11 @@ summary(coxfit.unweighted)
 ## ----------
 ## W/ sampling weights
 ## cf the svydesign call in the docs for survey::nhanes
+df$bmi.cat.inter = interaction(df$bmi.cat, df$bmi.max.cat, drop = TRUE)
 design = svydesign(id = ~ psu, strata = ~ stratum, weights = ~ sample.weight, 
 				   nest = TRUE, 
 				   data = {df %>% filter(!smoker, age.months >= 50*12, age.months < 85*12)})
 coxfit.weighted = svycoxph(Surv(age.months, mort.status == 'deceased') ~ 
-						   	bmi.cat * bmi.max.cat, 
+						   	bmi.cat.inter, 
 				 design = design)
-
+summary(coxfit.weighted)
