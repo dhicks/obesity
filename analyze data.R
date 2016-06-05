7ff76ccb1c61ff7e3b3de92584d1802d79438b8b
Dan Hicks <hicks.daniel.j@gmail.com>
2016-06-05 16:02:11 -0500

    analysis using the survey package

--- analyze data.R
+++ analyze data.R
@@ -2,69 +2,100 @@
 
 library(cowplot)
 library(dplyr)
-library(survival)
-load('2016-05-19.RData')
+library(mgcv)
+#library(survival)
+library(survey)
+load('2016-05-24.RData')
 
-## TODO: the models seem to be overly precise with sample weights.  need to get help!
+## TODO: df -> dataf
 
-## Filter down to complete cases
-df = df[complete.cases(df), ]
-## Filter down to non-smokers, between 50 and 85, not underweight
-df = df %>% filter(!smoker, age.months >= 50*12, age.months < 85*12, 
-				   bmi.cat != 'underweight')
-## Table by mortality status
-table(df$mort.status)
+## Drop one entry with NA id
+df = df %>% filter(!is.na(id))
+## Move to a survey design object
+design_unfltd = svydesign(id = ~ psu, strata = ~ stratum, weights = ~ sample.weight, 
+				   nest = TRUE, 
+				   data = df)
+## Filtered dataset: nonsmokers, 50-84 years old, not underweight
+design = subset(design, (!smoker) & (age.months >= 50*12) & (age.months < 85*12) 
+					 & (bmi.cat != 'underweight')
+					 )
+## Build interaction term
+design = update(design, bmi.cat.inter = interaction(bmi.cat, bmi.max.cat, drop = TRUE))
 
 ## ----------
-## Survival analysis
-## Define the survival object
-survobj = with(df, Surv(age.months, mort.status == 'deceased'))
-## Curves by BMI category at examination
-# fit = survfit(survobj~bmi.cat, data = df)
-# survdiff(survobj~bmi.cat, data = df)
-
-## Cox proportional hazards models
-## Set this option to get desired behavior w/ ordered factors
-#options(contrasts = c("contr.treatment", "contr.poly"))
-## Breaking BMI and max BMI into categories
-coxfit.cat = coxph(survobj ~ sex + race.ethnicity + education +
-				   bmi.cat + bmi.max.cat,
-				   data = df, weights = sample.weight)
+## Weighted survival analysis, categorical BMI
+## Fit the model
+coxfit.cat = svycoxph(Surv(age.months, mort.status == 'deceased') ~ 
+				  			sex + race.ethnicity + education +
+				  			bmi.cat.inter, 
+						   design = design)
 summary(coxfit.cat)
-## Continuous BMI and max BMI
-coxfit.cont = coxph(survobj ~ sex + race.ethnicity + education + 
-						bmi + bmi.max, 
-					data = df, weights = sample.weight)
+
+## Weighted survivial analysis, continuous BMI
+## Fit the model
+coxfit.cont = svycoxph(Surv(age.months, mort.status == 'deceased') ~
+					   	sex + race.ethnicity + education + 
+					   	bmi * bmi.max, 
+					   design = design)
 summary(coxfit.cont)
 
+# ## ----------
+# ## Unweighted survival analysis
+# ## Define the survival object
+# survobj = with(df, Surv(age.months, mort.status == 'deceased'))
+# ## Curves by BMI category at examination
+# # fit = survfit(survobj~bmi.cat, data = df)
+# # survdiff(survobj~bmi.cat, data = df)
+# 
+# ## Cox proportional hazards models
+# ## Set this option to get desired behavior w/ ordered factors
+# #options(contrasts = c("contr.treatment", "contr.poly"))
+# ## Breaking BMI and max BMI into categories
+# coxfit.cat = coxph(survobj ~ sex + race.ethnicity + education +
+# 				   bmi.cat + bmi.max.cat,
+# 				   data = df)
+# summary(coxfit.cat)
+# ## Continuous BMI and max BMI
+# coxfit.cont = coxph(survobj ~ sex + race.ethnicity + education + 
+# 						bmi + bmi.max, 
+# 					data = df)
+# summary(coxfit.cont)
+
 
 ## ----------
-## LOESS analysis
+## LOESS
 # ggplot(data = df, aes(x = bmi, y = as.numeric(mort.status) - 1,
 # 					  color = race.ethnicity,
 # 					  shape = sex, linetype = sex)) +
 # 	geom_point(position = position_jitter(height = .25), alpha = .1) +
 # 	stat_smooth(alpha = .2) +
-# 	geom_vline(xintercept = bmi_breaks, color = 'grey') +
+# 	#geom_vline(xintercept = bmi_breaks, color = 'grey') +
 # 	scale_y_continuous(name = 'mortality', breaks = c(0,1),
 # 					   labels = c('alive', 'deceased')) +
 # 	scale_x_continuous(limits = c(NA, 50)) +
 # 	facet_wrap(~ race.ethnicity)
 
 ## ----------
+## GAM
+## TODO: try something like this: http://stackoverflow.com/questions/16569299/r-svyglm-prediction-with-spline-function
+
+# gamfit = gam(mort.status ~ bmi.cat * bmi.max.cat + 
+# 			 	sex + age.months + race.ethnicity + education, 
+# 			 data = df)
+
+## ----------
 ## Binomial regression
-# fit1 = glm(mort.status ~ bmi,
-# 		   family = binomial,
-# 		   data = df)
-# summary(fit1)
-# 
-# fit2 = glm(mort.status ~ bmi + sex + age.months + race.ethnicity, 
-# 		   family = binomial, 
-# 		   data = df)
-# summary(fit2)
-# 
-# fit3 = glm(mort.status ~ bmi + bmi.max + sex + age.months + race.ethnicity, 
-# 		   family = binomial, 
-# 		   data = df)
-# summary(fit3)
-# exp(fit3$coefficients)
+## Categorical BMI
+binomfit.cat = svyglm(mort.status ~ bmi.cat.inter + 
+				   	sex + age.months + race.ethnicity + education, 
+				  family = quasibinomial(), 
+				  design = design)
+summary(binomfit.cat)
+
+## Continuous BMI
+binomfit.cont = svyglm(mort.status ~ bmi * bmi.max + 
+						sex + age.months + race.ethnicity + education, 
+					family = quasibinomial(), 
+					design = design)
+summary(binomfit.cont)
+
